37c7e5fdffa3d7df9ffc03cd194ac2089f522e37
xQesh <77618479+xQesh@users.noreply.github.com>
2025-05-23 17:48:45 +0200

    Dodano wyświetlanie szczegółów pojedyńczych tasków

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import json
 DEV_MODE = True
 
 class ConfigHandler:
-    def load():
+    def get():
         try:
             with open('config.json', 'r') as cfg:
                 return json.load(cfg)
@@ -23,9 +23,30 @@ class ConfigHandler:
             print('Config file editing error')
 
 class TasksHandler:
-    def load():
+    #def __init__(self):
+    #    self.get()
+
+    @staticmethod
+    def get():
         with open('tasks.json', 'r') as tasks:
-            return json.load(tasks)        
+            return json.load(tasks)
+        
+    def detailed_mode(self, id):
+            for task_id, info in self.get().items():
+                if task_id == id:
+                    os.system('cls')
+                    print('ID:', id)
+                    print('TITLE:', info["title"])
+                    print('DESCRIPTION:', info["description"])
+                    if info["completed"]:
+                        status = 'completed'
+                    else:
+                        status = 'uncompleted'
+                    print('STATUS:', status)
+                    return
+
+            if DEV_MODE:
+                print('ID that you chose dont match any task')
 
     def add():
         pass
@@ -39,22 +60,32 @@ class TasksHandler:
 class Main:
     def __init__(self):
         os.system('cls')          
-        username = ConfigHandler.load()['username']
+        username = ConfigHandler.get()['username']
         print(f'Welcome {username}!')
         print('Your tasks:')
         print("ID       TITLE      STATUS")
 
-        tasks = TasksHandler.load()
+        tasks = TasksHandler.get()
 
         for id, info in tasks.items():    
             if info["completed"]: 
                 status = "COMPLETED" 
             else: 
                 status = "UNCOMPLETED"
-            print(f'{id} - {info["title"]} - {status}') 
+            print(f'{id} - {info["title"]} - {status}')
+
+
+        while True:
+            print('Choose task by ID or quit')
+            choice = input('Option: ')
+            if choice == 'quit':
+                sys.exit()
+            else:
+                handler = TasksHandler()
+                handler.detailed_mode(choice)
 
 class Launcher:
-    config = ConfigHandler.load()
+    config = ConfigHandler.get()
     
     if config['firstlaunch'] or config['username'] == "":
         config['username'] = input("Name: ")
